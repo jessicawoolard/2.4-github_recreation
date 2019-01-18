d8bc2ef558987dcd8e81a148a9c6738923bbb1dd
Jessica Woolard <jessicawoolard@Jessicas-MacBook-Pro.local>
2019-01-18 15:02:00 -0500

    more html and github api added

--- app.py
+++ app.py
@@ -3,9 +3,25 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+import requests
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+
+    response = requests.get('https://api.github.com/users/jessicawoolard')
+
+    data = response.json()
+    # print(data)
+
+    github_infomation = data['login']
+    print(github_infomation)
+
+
+
+
+# @app.route('/repos')
+
+# https://api.github.com/users/jessicawoolard/repos

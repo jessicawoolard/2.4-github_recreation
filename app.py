3cfafc5d5b958a7b4db0a4443d59847112b1cadf
Jessica Woolard <jessicawoolard@Jessicas-MacBook-Pro.local>
2019-01-21 00:48:15 -0500

    start of python, fixed HTML/CSS

--- app.py
+++ app.py
@@ -12,16 +12,38 @@ app = Flask(__name__)
 def index():
 
     response = requests.get('https://api.github.com/users/jessicawoolard')
+    user = response.json()
 
-    data = response.json()
-    # print(data)
+    repos_response = requests.get(user['repos_url'])
+    repos = repos_response.json()
 
-    github_infomation = data['login']
-    print(github_infomation)
+    # print(repos)
+
+    context = {
+        'profile': user,
+        'repos': repos
+    }
+
+
+    return render_template('index.html', **context)
 
 
 
 
-# @app.route('/repos')
+@app.route('/followers')
+def followers():
 
-# https://api.github.com/users/jessicawoolard/repos
+    response = requests.get('https://api.github.com/users/jessicawoolard')
+    user = response.json()
+
+    followers_response = requests.get(user['followers_url'])
+    followers = followers_response.json()
+
+
+    context = {
+        'profile': user,
+        'followers': followers
+    }
+
+
+    return render_template('followers.html', **context)
